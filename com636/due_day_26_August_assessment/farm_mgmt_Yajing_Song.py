d7e24d612628a22838be0a0df3d6c777a1238908
Yajing-Song-1162200 <Yajing.Song@lincolnuni.ac.nz>
2024-08-24 15:07:30 +1200

    feat: Add code examples for try-except, while loops, validation, function parameters, tuples, nested arrays, dictionaries, strings, lists, for loops, and function definitions

--- com636/due_day_26_August_assessment/farm_mgmt_Yajing_Song.py
+++ com636/due_day_26_August_assessment/farm_mgmt_Yajing_Song.py
@@ -1,103 +1,11 @@
 # ============== TE WAIHORA FARM MANAGEMENT SYSTEM ==============
 # Student Name:Yajing Song
-# Student ID :
+# Student ID : 
 # ================================================================
  
 from datetime import datetime,timedelta     # datetime module is required for working with dates
 
 import farm_data    # Makes the variables and function in farm_data.py available in this code
-
-# {mob_name: [list of stock IDs]}
-mobs = {
-    "R1-2": [932, 343, 786, 801, 878, 947, 951, 901, 789, 899, 888],
-    "R3": [816, 810, 802, 800, 833, 777, 798],
-    "Bulls": [121, 196]
-}
-
-R1 = []
-R3 = []
-Bulls = []
-
-# [id, mob_name, birth_date, age_yrs ,weight]
-stock = [
-	[816, 'R3', datetime(2022,7,15), 2, 558.2],
-	[932, 'R1-2', datetime(2023,8,10), 1, 284.5],
-	[343, 'R1-2', datetime(2023,9,1), 0, 269.1],
-	[810, 'R3', datetime(2022,8,12), 2, 538.6],
-	[786, 'R1-2', datetime(2023,7,16), 1, 302],
-	[801, 'R1-2', datetime(2023,9,12), 0, 261.4],
-	[121, 'Bulls', datetime(2022,8,4), 2, 842.3],
-	[878, 'R1-2', datetime(2023,9,2), 0, 268.4],
-	[947, 'R1-2', datetime(2023,8,30), 0, 270.5],
-	[802, 'R3', datetime(2022,8,25), 1, 529.5],
-	[951, 'R1-2', datetime(2023,7,25), 1, 295.7],
-	[901, 'R1-2', datetime(2023,8,4), 1, 288.7],
-	[800, 'R3', datetime(2022,9,6), 1, 521.1],
-	[789, 'R1-2', datetime(2023,9,14), 0, 260],
-	[899, 'R1-2', datetime(2023,8,24), 0, 274.7],
-	[833, 'R3', datetime(2022,9,9), 1, 519],
-	[196, 'Bulls', datetime(2023,7,4), 1, 522],
-	[777, 'R3', datetime(2022,9,20), 1, 511.3],
-	[888, 'R1-2', datetime(2023,9,23), 0, 253.7],
-	[798, 'R3', datetime(2022,8,13), 2, 537.9]
-]
-def stock_group(group,name):
-    print(f"{name:^50}")
-    print(f"{'ID':<5}  {"mob_name":<10}  {"birth_date":<10}  {"age_yrs":<10}  {"weight":<5}")
-    print("--------------------------------------------------")
-    for item in group:
-        print(f"{item[0]:<5}  {item[1]:<10}  {item[2].strftime('%d-%m-%Y'):<10}  {item[3]:<10}  {item[4]:<5}")
-
-
-for item in stock:
-    if item[1] == "R1-2":
-        R1.append(item)
-    elif item[1] == "R3":
-        R3.append(item)
-    elif item[1] == "Bulls":
-        Bulls.append(item)
-
-stock_group(R1,"R1-2")
-stock_group(R3,"R3")
-stock_group(Bulls,"Bulls")
-
-    
-# {paddock_name: {paddock_details}}
-paddocks = {
-    "Corner": {'area':1.4, 'dm/ha': 1850, 'total dm':2590, 'mob':None, 'stock num': 0},
-    "Back": {'area':0.8, 'dm/ha': 2150, 'total dm':1760, 'mob':'R1-2', 'stock num': 11},
-    "Front": {'area':1.6, 'dm/ha': 2000, 'total dm':3200, 'mob':'R3', 'stock num': 7},
-    "Barn": {'area':0.5, 'dm/ha': 2200, 'total dm':1100, 'mob':'Bulls', 'stock num': 2}
-}
-
-
-def paddocks_dictionary():
-    for key in sorted(paddocks):
-        print(f"{key:^50}")
-        print(f"{'area':<5}  {"dm/ha":<10}  {"total dm":<10}  {"mob":<10}  {"stock num":<5}")
-        print("-----------------------------------------------------")
-        value = paddocks[key]
-        outter_value = value.values()
-        # for outter_value in inner_value:
-        # conver dic values to list
-        outter_value = list(outter_value)
-        print(f"{outter_value[0]:<5}  {outter_value[1]:<10}  {outter_value[2]:<10}  {str(outter_value[3]):<10}  {outter_value[4]:<5}")
-
-paddocks_dictionary()
-
-
-    # print(f"{'Paddock Name':<10}  {'Area':<5}  {'DM/ha':<5}  {'Total DM':<5}  {'Mob':<5}  {'Stock Num':<5}")
-    # print(f"{key:<10}  {paddocks[key]['area']:<5}  {paddocks[key]['dm/ha']:<5}  {paddocks[key]['total dm']:<5}  {paddocks[key]['mob']:<5}  {paddocks[key]['stock num']:<5}")
-
-
-
-
-# for item in stock:
-
-# sorted_paddocks = sorted(paddocks.items())
-# print("Paddock Name  Area  DM/ha  Total DM  Mob  Stock Num")
-# print(f"{'Paddock Name':<10}  {'Area':<5}  {'DM/ha':<5}  {'Total DM':<5}  {'Mob':<5}  {'Stock Num':<5}")
-# print(sorted_paddocks)
 
 
 # Global variable values that can be referred to throughout your code.  
@@ -137,21 +45,105 @@ def list_all_stock():
 def list_stock_by_mob():
     """
     Lists stock details (including birth date), grouped by mob name."""
+    # Get the mob names from the mobs dictionary
+    mob_keys = mobs.keys()
+    # for each mob name, display the stock which belong to that mob
+    for mob in mob_keys:
+        print("\nMob:", mob)
+        format_str = "{: <8} {: <8} {: <5} {: ^10} {: <10}"            # Use the same format_str for column headers and rows to ensure consistent spacing. 
+        display_formatted_row(["Mob","ID","Birth Date","Age","Weight",],format_str)     # Use the display_formatted_row() function to display the column headers with consistent spacing
+        for animal in stock:
+            if animal[1] == mob:
+                id = animal[0]
+                mob = animal[1]
+                age = animal[3]
+                weight = animal[4]
+                birth_date = animal[2].strftime("%d/%m/%Y")
+                display_formatted_row([mob,id,birth_date,age,weight],format_str)     # Use the display_formatted_row() function to display each row with consistent spacing
+    print("\nStock grouped by mob:", mob_keys)
     input("\nPress Enter to continue.")
 
 def list_paddock_details():
     """
     List the paddock names and all details."""
+    for key in sorted(paddocks):
+        print(f"\n{key:^50}") 
+        print(f"{"area":<5}  {"dm/ha":^10}  {"total dm":<10}  {"mob": ^10}  {"stock num":<5}")
+        value = paddocks[key]
+        outter_value = value.values()
+        # for outter_value in inner_value:
+        # conver dic values to list
+        outter_value = list(outter_value)
+        print(f"{outter_value[0]:<5}  {outter_value[1]:^10}  {outter_value[2]:^8}  {str(outter_value[3]): ^13}  {outter_value[4]:^9}")
     input("\nPress Enter to continue.")
+
 
 def move_mobs_between_paddocks(paddocks):
     """
     Change which paddock each mob is in. """
+    # Let the user choose a mob from "mobs" dictionary(user can input number/letter to choose their choice) and to move to a new paddock from "paddocks" dictionary which is not have any mob in it.
+    # If the paddock has a mob in it, the user should be informed and asked to choose another paddock.
+    mobs_keys = mobs.keys()
+    paddocks_keys = paddocks.keys()
+    #user can choose a number which is related to the mob name to move the mob to a new paddock
+    print ("\nPlease choose a number of the mob to move to a new paddock:")
+    print("* * * This is a paddock manu * * *")
+    print("1 : R1-2")
+    print("2 : R3")
+    print("3 : Bulls")
+    mob_choice = input("Enter the number of the mob you want to move: ")
+    #user can choose a number which is related to the paddock name to move the mob to a new paddock
+    print("\n* * * This is a paddock manu * * *")
+    # find all the paddocks that do not have a mob in it
+    print("\nPlease choose a number of the paddock to move the mob to:")
+    for index,key in enumerate(paddocks_keys):
+        if paddocks[key]['mob'] == None:
+            print(f"{index + 1} : {key}")
+    paddock_choice = input("Enter the number of the paddock you want to move the mob to: ")
+    # CHange the target paddock to the mob
+    paddock_choice = int(paddock_choice)
+    mob_choice = int(mob_choice)
+    paddock_choice = paddock_choice - 1
+    mob_choice = mob_choice - 1
+    paddock_key = paddocks_keys[paddock_choice]
+    mob_key = list(mobs_keys)[mob_choice]
+    # excute the move action
+    paddocks[paddock_key]['mob'] = mob_key
+    print(f"\nMob {mob_key} has been moved to paddock {paddock_key}")
+
+    input("\nPress Enter to continue.")
+
+   
+
+   
+   
+
+    
+
+    
+
+
+
+
+
+   
+   
+
+   
+   
+    
+
+     
+
+    
+   
+    
     pass  # REMOVE this line once you have some function code (a function must have one line of code, so this temporary line keeps Python happy so you can run the code)
 
 def add_new_stock(stock):
     """
     Add a new animal to the stock list."""
+
     pass  # REMOVE this line once you have some function code (a function must have one line of code, so this temporary line keeps Python happy so you can run the code)
 
 def move_to_next_day(stock, paddocks):
@@ -204,11 +196,11 @@ while response.upper() != "X":
     # The user can enter "X" to exit the program.
     elif response.upper() == "X":
         print("=== Exiting the program ===")
-        break
+        break   
     elif response.upper() != "X":
         print("\n*** Invalid response, please try again (enter 1-6 or X)")
 
     print("")
 
-print("\n=== Thank you for using Te Waihora Farm Management System! ===\n")
 
+print("\n=== Thank you for using Te Waihora Farm Management System! ===\n")
